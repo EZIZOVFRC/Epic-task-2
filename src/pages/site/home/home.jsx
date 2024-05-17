eaed404e75e162331484880e83b599fea4ee75df
EZIZOVFRC <TU853LXMP@CODE.EDU.AZ>
2024-05-17 20:04:47 +0400

    fr21

--- src/pages/site/home/home.jsx
+++ src/pages/site/home/home.jsx
@@ -2,16 +2,18 @@
     import { Helmet } from 'react-helmet'
 import SimpleSlider from './Slider/Slider';
 import Cards from '../Cards/Cards';
+import { useContext } from 'react';
+import MainContext from '../../../context/context';
     
     const Home = () => {
-  
+        const {data,setdata} = useContext(MainContext)
         return (
         <>
                 <Helmet>
                     <title>Home</title>
                 </Helmet>
                 <SimpleSlider/>
-                <Cards/>
+                <Cards data={data}/>
         </>
         )
     }
